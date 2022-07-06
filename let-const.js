79cebe335a5a333f864cf65fc5129095e3f3ce1c
hasan <mdhasanhk.98@gmail.com>
2022-07-06 03:37:57 -0400

    letconst

---
+++ let-const.js
@@ -0,0 +1,22 @@
+const hubby = "raju molla";
+console.log(hubby);
+
+const numbers = [20, 40, 56]
+numbers[1] = 5;
+numbers.push(10);
+console.log(numbers);
+
+const nayak = {name: "shakib khan", age: 20,}
+console.log(nayak);
+
+let patient = "Hasan khan";
+patient = "rohim";
+console.log(patient);
+
+let sum = 0;
+for(var i = 0; i < 10; i++) {
+    sum = sum + i;
+    
+}
+console.log(i);
+
