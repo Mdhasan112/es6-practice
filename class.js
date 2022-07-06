6be211f44f0a2fa4010d1ac51fd890e619695a4d
hasan <mdhasanhk.98@gmail.com>
2022-07-06 15:14:25 -0400

    class

---
+++ class.js
@@ -0,0 +1,12 @@
+class student {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+        this.school = "MAHM"
+    }
+}
+
+const student1 = new student(5, "hasan");
+const student2 = new student(2, "raju");
+
+console.log(student1, student2);
