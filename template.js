1571f5c7474f2999c34cdd49b1f0c1cbf6350224
hasan <mdhasanhk.98@gmail.com>
2022-07-06 04:28:15 -0400

    template.js

---
+++ template.js
@@ -0,0 +1,5 @@
+const firstName = "justin"
+const lastName = "beiber"
+const fullName = firstName + " " + lastName + "" + "is very good man";
+const fullName2 = `${firstName} ${lastName} is a very bad man.`
+console.log(fullName2)
