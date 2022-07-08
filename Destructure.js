ba5389d215f4fdab45d3314b449d75344a5701b0
hasan <mdhasanhk.98@gmail.com>
2022-07-08 16:06:11 -0400

    distructure

---
+++ Destructure.js
@@ -0,0 +1,9 @@
+const person = {name: "hasan khan", age: 25, job: "freelancher", gfName: "sakira", phone: "01797999949"}
+
+const {phone} = person;
+const gfName = person.gfName;
+const age = person.age;
+
+console.log(gfName, age, phone);
+console.log(gfName);
+console.log(gfName);
