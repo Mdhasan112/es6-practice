207a4c9b8978a141b58327202e9ece0ffa8e5e69
hasan <mdhasanhk.98@gmail.com>
2022-07-08 16:21:28 -0400

    destructure

--- Destructure.js
+++ Destructure.js
@@ -1,9 +1,15 @@
 const person = {name: "hasan khan", age: 25, job: "freelancher", gfName: "sakira", phone: "01797999949"}
 
-const {phone} = person;
-const gfName = person.gfName;
-const age = person.age;
+const {gfName, phone, age,} = person;
+
+// const gfName = person.gfName;
+// const age = person.age;
 
 console.log(gfName, age, phone);
 console.log(gfName);
 console.log(gfName);
+
+const friendsName = ["nayem", "rofiq", "raju", "basar"]
+
+const [firstFriend, second, ...restFriend] = friendsName;
+console.log(firstFriend,  second, restFriend);
